93b1fefb3fc6dc97bb3b006cc2daa223ce009d75
Leogrimes25 <vleonardo203@gmail.com>
2024-04-04 11:55:58 -0300

    10

---
+++ dressToImpressShop_viniciusOliveira/src/main/java/com/example/dressToImpressShop_viniciusOliveira/controller/ListagemProdutosController.java
@@ -0,0 +1,43 @@
+package com.example.dressToImpressShop_viniciusOliveira.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class ListagemProdutosController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @RequestMapping("/produtos")
+    public ModelAndView listarProdutos() {
+        ModelAndView modelAndView = new ModelAndView("produtos");
+        try {
+            List<Map<String, Object>> produtos = jdbcTemplate.queryForList("SELECT * FROM produtos");
+            modelAndView.addObject("produtos", produtos);
+        } catch (DataAccessException e) {
+            modelAndView.setViewName("erro");
+            modelAndView.addObject("mensagem", "Erro ao recuperar os pedidos do banco de dados.");
+        }
+
+
+        return modelAndView;
+    }
+
+    @GetMapping("/buscar")
+    public String buscarProdutosPorNome(@RequestParam("nome") String nome, Model model) {
+        String sql = "SELECT * FROM produtos WHERE nome_produto = ?";
+        List<Map<String, Object>> produtos = jdbcTemplate.queryForList(sql, nome);
+        model.addAttribute("produtos",produtos);
+        return "produtos";
+    }
+}
