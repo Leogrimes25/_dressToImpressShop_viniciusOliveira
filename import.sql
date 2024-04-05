70ea5e7fd0d3d57af727a94922a07bd1681f7499
Vinicius Leonardo de Oliveira <137174644+Leogrimes25@users.noreply.github.com>
2024-04-05 17:53:51 -0300

    Update import.sql

--- import.sql
+++ import.sql
@@ -6,4 +6,4 @@ VALUES
 ('GPU','AMD RADEON','RX 6600 EAGLE','8GB','900.00','1400.00','placa-de-video-gigabyte-amd-radeon-rx-6600.jpg','2024-04-04'),
 ('GPU','Nvidia Gigabyte','GeForce RTX 4060 EAGLE OC','8GB','1400.00','2000.00','placa-de-video-gigabyte-nvidia-geforce-rtx-4060.jpg','2024-04-04'),
 ('GPU','Nvidia MSI','GTX 1650 Ventus XS OC','4GB','500.00','850.00','placa-de-video-msi-nvidia-geforce-gtx-1650.jpg','2024-04-04'),
-('GPU','Palit','GeForce RTX 4080 Super Gaming Pro','16GB','600.00','7500.00','placa-de-video-palit-geforce-rtx-4080-super-gaming-pro-oc','2024-04-04');
+('GPU','Palit','GeForce RTX 4080 Super Gaming Pro','16GB','600.00','7500.00','placa-de-video-palit-geforce-rtx-4080-super-gaming-pro-oc.jpg','2024-04-04');
